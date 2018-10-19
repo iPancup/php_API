7bd057a8a3c38b443a19c1aba6b736d8d8d55599
iPancup <ivanyusuf642@gmail.com>
2018-10-19 21:57:45 +0700

    first commit

---
+++ tabel_kelas.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 19, 2018 at 04:47 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tugas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabel_kelas`
+--
+
+CREATE TABLE `tabel_kelas` (
+  `id` int(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `level` varchar(50) NOT NULL,
+  `fullname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tabel_kelas`
+--
+
+INSERT INTO `tabel_kelas` (`id`, `username`, `password`, `level`, `fullname`) VALUES
+(1, 'ivan', '123', 'admin', 'ivan yusuf'),
+(2, 'damas', '123', 'user', 'kristian damas'),
+(3, 'rafi', '123', 'user', 'raffdani'),
+(4, 'kukuh', '123', 'user', 'kukuh adji'),
+(5, 'ardha', '123', 'user', 'ardhana');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
